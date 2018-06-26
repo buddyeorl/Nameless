31eb55757f2f2d9823165860327935d311e130db
Alexander A Lizarraga <alexander_lizarraga@ymail.com>
2018-06-26 00:57:54 -0400

    created a full stack app for what the fork

--- server.js
+++ server.js
@@ -1,88 +1,36 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-var firebase = require('firebase').initializeApp({
-	"serviceAccount": "forkOff-b754102139e3.json",
-	"databaseURL": "https://forkoff-b5d5b.firebaseio.com"
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 3001;
+
+// BodyParser makes it possible for our server to interpret data sent to it.
+// The code below is pretty standard.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static('app/public'));
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+	console.log("Food Matcher App listening on PORT: " + PORT);
 });
-
-//
-var messageWatson;
-var fs = require('fs');
-
-// var visual_recognition = watson.visual_recognition({
-//     api_key: '3511cb92faf71512889a8004b4e0eea17618cdf8', //alex
-//     // api_key: '6de8f853781dde067ae4c1ebde94f4579a4e8a6c', //Charlie
-//     version: 'v3',
-//     version_date: '2016-05-20'
-// });
-
-var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
-
-var visualRecognition = new VisualRecognitionV3({
-	version: 'v3',
-	api_key: '3511cb92faf71512889a8004b4e0eea17618cdf8',
-	version_date: '2016-05-20'
-});
-
-
-var ref = firebase.database().ref().child('node-client');
-var logsRef = ref.child('images');
-var messagesRef = ref.child('messages');
-
-
-logsRef.orderByKey().limitToLast(1).on('child_added', function (snap) {
-	console.log('added', snap.val());
-});
-
-logsRef.on('child_removed', function (snap) {
-	console.log('removed', snap.val());
-});
-
-logsRef.child('TestingImage').on('value', function (snap) {
-	imageToUse = snap.val();
-	watsoncheck(imageToUse);
-	console.log(imageToUse);
-	console.log('changed', snap.val());
-});
-logsRef.on('value', function (snap) {
-	console.log('value', snap.val());
-});
-//});
-
-
-function watsoncheck(images) {
-	var watson = require('watson-developer-cloud');
-	var fs = require('fs');
-
-	var params = {
-		parameters: { 'url': images, 'classifier_ids': ["Buddhaxshand_1013586818", "default"] }
-	};
-
-
-	//THE LINES BELOW WILL CONSOLE.LOG THE LIST OF CUSTOM CLASSIFIERS 
-	visualRecognition.listClassifiers({},
-		function (err, response) {
-			if (err)
-				console.log(err);
-			else
-				console.log(JSON.stringify(response, null, 2));
-		}
-	);
-	visualRecognition.classify(params, function (err, res) {
-		if (err)
-			console.log(err);
-		else
-			messageWatson = res;
-		console.log(messageWatson);
-		var message = { images: messageWatson }; //, timestamp: new Date().toString()
-		var ref = firebase.database().ref().child('node-client');
-		setMessages(message);
-		message = 0
-	});
-
-}
-
-
-
-function setMessages(message) {
-	ref.child('messages').update(message);
-}
