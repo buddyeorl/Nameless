47adbb731abd689100c7da721397c1ac4e89c45c
Alexander A Lizarraga <alexander_lizarraga@ymail.com>
2018-10-21 00:12:20 -0400

    changed APIKEY to IMA apikey to follow changes from IBM watson

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -18,26 +18,17 @@ module.exports = function (app) {
 	var messageWatson;
 	var fs = require('fs');
 
-	// var visual_recognition = watson.visual_recognition({
-	//     api_key: '3511cb92faf71512889a8004b4e0eea17618cdf8', //alex
-	//     // api_key: '6de8f853781dde067ae4c1ebde94f4579a4e8a6c', //Charlie
-	//     version: 'v3',
-	//     version_date: '2016-05-20'
-	// });
-
 	var VisualRecognitionV3 = require('watson-developer-cloud/visual-recognition/v3');
 
 	var visualRecognition = new VisualRecognitionV3({
-		version: 'v3',
-		api_key: '3511cb92faf71512889a8004b4e0eea17618cdf8',
-		version_date: '2016-05-20'
+		url: 'https://gateway.watsonplatform.net/visual-recognition/api',
+		version: '2018-03-19',
+		iam_apikey: '2Hv41AInWvCdvRJUhg_04W46-Go8ZpYAMe8f-LcMZ5Ko',
 	});
-
 
 	var ref = firebase.database().ref().child('node-client');
 	var logsRef = ref.child('images');
 	var messagesRef = ref.child('messages');
-
 
 	logsRef.orderByKey().limitToLast(1).on('child_added', function (snap) {
 		console.log('added', snap.val());
@@ -51,37 +42,26 @@ module.exports = function (app) {
 		imageToUse = snap.val();
 		watsoncheck(imageToUse);
 		console.log(imageToUse);
-		console.log('changed', snap.val());
+		console.log('changed', imageToUse);
 	});
-	logsRef.on('value', function (snap) {
-		console.log('value', snap.val());
-	});
-	//});
 
 
 	function watsoncheck(images) {
-		var watson = require('watson-developer-cloud');
-		var fs = require('fs');
-
+		console.log("sent to watson" + images)
 		var params = {
-			parameters: { 'url': images }
+			'url': images
 		};
 
-
-		//THE LINES BELOW WILL CONSOLE.LOG THE LIST OF CUSTOM CLASSIFIERS 
-		visualRecognition.listClassifiers({},
-			function (err, response) {
-				if (err)
-					console.log(err);
-				else
-					console.log(JSON.stringify(response, null, 2));
-			}
-		);
 		visualRecognition.classify(params, function (err, res) {
-			if (err)
+			if (err) {
+				console.log('ERROR FOUND HERE HERE');
+				console.log(err.code);
 				console.log(err);
-			else
+			}
+			else {
+				console.log(JSON.stringify(res));
 				messageWatson = res;
+			}
 			console.log(messageWatson);
 			var message = { images: messageWatson }; //, timestamp: new Date().toString()
 			var ref = firebase.database().ref().child('node-client');
@@ -92,8 +72,8 @@ module.exports = function (app) {
 	}
 
 
-
 	function setMessages(message) {
+		console.log(JSON.stringify(message));
 		ref.child('messages').update(message);
 	}
 };
